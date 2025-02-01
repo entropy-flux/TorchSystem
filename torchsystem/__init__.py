d0518fb7462a9f094acee6643249888cf6173108
mr-mapache <eric.m.cardozo@mi.unc.edu.ar>
2025-02-01 19:08:06 +0000

    Remove usless folder / Update docs

--- torchsystem/__init__.py
+++ torchsystem/__init__.py
@@ -1,3 +1,3 @@
-from torchsystem.aggregate import Aggregate as Aggregate
+from torchsystem.domain import Aggregate as Aggregate
 from torchsystem.compiler import Compiler as Compiler
 from torchsystem.depends import Depends as Depends
--- torchsystem/registry.py
+++ torchsystem/registry.py
@@ -5,3 +5,4 @@ from mlregistry.accessors import gethash as gethash
 from mlregistry.accessors import getname as getname
 from mlregistry.accessors import sethash as sethash
 from mlregistry.accessors import setname as setname
+from mlregistry.accessors import getmetadata as getmetadata
