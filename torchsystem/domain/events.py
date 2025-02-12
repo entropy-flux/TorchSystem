9cfddac1375fef6b7f0bacdb4b7693a7fb8e4f02
mr-mapache <eric.m.cardozo@mi.unc.edu.ar>
2025-02-12 00:00:54 +0000

    Update License Block

--- torchsystem/domain/events.py
+++ torchsystem/domain/events.py
@@ -10,9 +10,11 @@
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
-# limitations under the License.
+# limitations under the License. 
+#
+# 
+# For inquiries, visit the documentation at mr-mapache.github.io/torch-system/
 
-from typing import Literal 
 from typing import Optional
 from typing import overload
 from typing import Sequence
