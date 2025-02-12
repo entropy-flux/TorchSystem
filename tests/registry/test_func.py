f3f9ef0cebc6fdaa56c258f33e3215f52be0d65d
mr-mapache <eric.m.cardozo@mi.unc.edu.ar>
2025-02-12 01:02:40 +0000

    Remove mlregistry

---
+++ tests/registry/test_func.py
@@ -0,0 +1,55 @@
+from torchsystem.registry import register
+from torchsystem.registry.accessors import sethash, setname
+from torchsystem.registry.accessors import getarguments, gethash, getname, getmetadata
+
+@register
+class Model:
+    def __init__(self, x: int, y: float, z, t: str = '5'):
+        self.x = x
+        self.y = y
+        self.z = z
+        self.t = t
+
+    def params(self):
+        return self.x, self.y, self.z, self.t
+
+@register('Criterion')
+class Loss:
+    def __init__(self, x: int, y: float, z, t: str = '5'):
+        self.x = x
+        self.y = y
+        self.z = z
+        self.t = t
+
+class Optimizer:
+    def __init__(self, x: int, y: float, z, t: str = '5'):
+        self.x = x
+        self.y = y
+        self.z = z
+        self.t = t
+
+def test_registration():
+    model = Model(1, 2.0, '3')
+    assert getname(model) == 'Model'
+    assert getarguments(model) == {'x': 1, 'y': 2.0, 'z': '3'}
+    assert gethash(model) == 'b12461be073bff9f5847f3f423767aa2'
+
+    assert model.params() == (1, 2.0, '3', '5')
+
+    loss = Loss(1, 2.0, '3')
+    assert getname(loss) == 'Criterion'
+    assert getarguments(loss) == {'x': 1, 'y': 2.0, 'z': '3'}
+    assert gethash(loss) != 'b12461be073bff9f5847f3f423767aa2'
+
+    register(Optimizer, excluded_args=[0])
+    optimizer = Optimizer(1, 2.0, '3')
+    assert getname(optimizer) == 'Optimizer'
+    assert getarguments(optimizer) == {'y': 2.0, 'z': '3'}
+
+    setname(model)
+    sethash(model)
+    assert getmetadata(model) == {
+        'name': 'Model',
+        'hash': 'b12461be073bff9f5847f3f423767aa2',
+        'arguments': {'x': 1, 'y': 2.0, 'z': '3'}
+    }
---
+++ tests/registry/test_nest.py
@@ -0,0 +1,36 @@
+from torchsystem.registry import register
+from torchsystem.registry.accessors import getarguments, gethash, getname
+
+@register
+class Module:
+    def __init__(self, x: int, y: float, z, t: str = '5'):
+        self.x = x
+        self.y = y
+        self.z = z
+        self.t = t
+
+    def params(self):
+        return self.x, self.y, self.z, self.t
+
+@register
+class Model:
+    def __init__(self, module: Module):
+        self.module = module
+
+@register
+class Dummy:
+    def __init__(self):
+        pass
+
+def test_nested():
+    module = Module(1, 2.0, '3', '4')
+    model = Model(module)
+
+    assert getarguments(module) == {'x': 1, 'y': 2.0, 'z': '3', 't': '4'}
+    assert getarguments(model) == {'module': {
+        'name': 'Module',
+        'arguments': {'x': 1, 'y': 2.0, 'z': '3', 't': '4'}}
+    }
+
+    model = Model(Dummy())
+    assert getarguments(model) == {'module': 'Dummy'}
